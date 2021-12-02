bc52359856d2a24db9eb9b9b6451173836dc2bfa
guspra <guspraa@gmail.com>
2021-12-02 15:53:24 +0800

    tambah return id monev saat create

--- application/controllers/Monev.php
+++ application/controllers/Monev.php
@@ -93,6 +93,7 @@ class Monev extends CI_Controller {
 							$resp = array('status' => 400,'message' =>  'Input form masih salah, silahkan coba lagi');
 						} else {
 								$resp = $this->MyModel->insert_to_table($this->table_name,$params);
+								$resp['id'] =  $this->MyModel->get_last_id($this->table_name);
 						}
 						json_output($respStatus,$resp);
 		        	}
